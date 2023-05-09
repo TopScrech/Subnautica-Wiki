5540daaea857fa858d3100e2cd52aa98934c7b09
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-05-10 01:59:11 +0200

    Initial Commit

---
+++ Subnautica Wiki/Subnautica_WikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Subnautica_WikiApp.swift
+//  Subnautica Wiki
+//
+//  Created by Sergei Saliukov on 10/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Subnautica_WikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
